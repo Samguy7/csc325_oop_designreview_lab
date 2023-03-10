131982321e116780b2ea107e6d80455bb37bba64
Samuel <sam@sams-mbp>
2023-03-09 23:30:14 -0500

    Todos Completed

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
@@ -3,6 +3,7 @@
  */
 
 package com.mycompany.csc325_oop_designreview_lab;
+import java.util.Scanner;
 
 /**
  *
@@ -20,22 +21,44 @@ public class MainClass {
                 // ToDo 8: The senior class should have a minimum of 85 credits  
 
 		// ToDo 7: Add a toString method for Student class
-		// ToDo 9: Add a toString method for Freshman class
-
-		Student std1= new Student("James", 20);
+		
+                // ToDo 8: Set the gpa of the student using the scanner and use
+                
+                // ToDo 9: Add a toString method for Freshman class 
                 // ToDo 10: Add a toString method for Senior class
-
-		Freshman std1= new Student("James", 20, 12); // name, age, credits
-
-                Senior std2 = new Student("John", 30, 90);
-
-		// ToDo 8: Set the gpa of the student using the scanner and user
-		// ToDo 11: Set the gpa of the student using the scanner and user
+		
+                // ToDo 11: Set the gpa of the student using the scanner and user
 		// 			input and then print the output.
+                short studentAge = 20;
+                Scanner inScanner = new Scanner(System.in);
+                double studentGPA = inScanner.nextDouble();
+                
+                
+                Student std1 = new Student("James", studentAge);
+                std1.setAddress("2 Dawn Lane");
+                std1.setGPA(studentGPA);
+                
+                
+                double freshmanGPA = inScanner.nextDouble();
+               
+		Freshman std2 = new Freshman("James", studentAge, 12); // name, age, credits
+                std2.setAddress("34 Yard Ave");
+                std2.setGPA(freshmanGPA);
 
-		System.out.println(std1);
+                double seniorGPA = inScanner.nextDouble();
+             
+                Short seniorAge = 30;
+                Senior std3 = new Senior("John", seniorAge, 70);
+                std3.setAddress("10 Forest Dr");
+                std3.setGPA(seniorGPA);
 
-                System.out.println(std2);
+		System.out.println(" " + std1);
+
+                System.out.println(" " + std2);
+
+                System.out.println(" " + std3);
+                
+                
 
 		// ToDo 9: add comments and explain your code
 		// ToDo 12: add comments and explain your code
